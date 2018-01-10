4fef169c28c7bb25d996adc5b07c64430186f8d9
root <root@packer-debian-8-amd64.droplet.local>
2018-01-10 12:45:21 +0000

    commit tri selection

--- class/tri_selection.class.php
+++ class/tri_selection.class.php
@@ -1,5 +1,33 @@
 <?php
 class TriSelection extends Tri
 {
+	public __construct($tab){
+		parent::__construct($tab, 'Selection');
+	}
+
+	public triTableau()
+	{
+		$lenght = count($this->tabNb);
+		$arrayTab = $this->tabNb;
+
+		for($i = 1; $i < $lenght - 1; $i++)
+		{
+			$min = $i;
+			for($j = $i + 1; $j < $lenght;$j++)
+			{
+				if ($arrayTab[$j] < $arrayTab[$min])
+				{
+					$min = $j;
+				}
+			}
+			if ($min != $i)
+			{
+				$tmp = $arrayTab[$i];
+				$arrayTab[$i] = $arrayTab[$min];
+				$arrayTab[$min] = $tmp;
+			}
+		}
+		
+	}
 }
 ?>
