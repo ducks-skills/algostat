0cc53e58e4d54ddb7f787dd56b8ad5b7c22dbbb5
ducks-skills <briand78410@gmail.com>
2018-01-10 15:01:59 +0100

    lol

--- class/triInsertion.class.php
+++ class/triInsertion.class.php
@@ -4,7 +4,24 @@ class triInsertion extends tri
 {
   public triTableau()
   {
-    
+    if (isset($this->tabNb[0]))
+    {
+      $i = 0;
+      $tmp = 0;
+      $this->setTabTri($this->tabNb);
+      $lenght = count($this->tabTri);
+      for ($i = 0, $i < $tabTri)
+      {
+        $tmp = $this->tabTri;
+        $j = $i;
+        while ($j > 0 && $this->tabTri[$j - 1] > $tmp)
+        {
+          $this->tabTri[$j] = $this->tabTri[$j - 1];
+          $j--;
+        }
+        $this->tabTri[$j] = $tmp;
+      }
+    }
   }
 }
 
