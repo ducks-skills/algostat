235400ff7ff1e7dca2fc9ac4437dcd08c3b66ff9
root <root@packer-debian-8-amd64.droplet.local>
2018-01-10 14:10:21 +0000

    lol

--- class/triBulle.class.php
+++ class/triBulle.class.php
@@ -2,33 +2,16 @@
 <?php
 class TriBulle extends Tri
 {
-	public __construct($tab){
+	public function __construct($tab){
 		parent::__construct($tab, 'Bulle');
 	}
 
-	public triTableau()
+	public function triTableau()
 	{
 		$lenght = count($this->tabNb);
 		$arrayTab = $this->tabNb;
-
-		for($i = 1; $i < $lenght - 1; $i++)
-		{
-			$min = $i;
-			for($j = $i + 1; $j < $lenght;$j++)
-			{
-				if ($arrayTab[$j] < $arrayTab[$min])
-				{
-					$min = $j;
-				}
-			}
-			if ($min != $i)
-			{
-				$tmp = $arrayTab[$i];
-				$arrayTab[$i] = $arrayTab[$min];
-				$arrayTab[$min] = $tmp;
-			}
-		}
 		
+		for ($i = $lenght)
 	}
 }
 ?>
