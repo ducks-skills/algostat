7e15c3e579e4c5af7293bb5702940d62f2e7d307
root <root@packer-debian-8-amd64.droplet.local>
2018-01-10 10:29:16 +0000

    Commit 2 : class tri

--- class/tri.class.php
+++ class/tri.class.php
@@ -1,2 +1,22 @@
 <?php
+
+class Tri
+{
+	private $timeExec;
+	private $chaineNb;
+	private $chaineTri;
+	private $name;
+
+	public function __construct(){}
+
+	public getTimeExec()  	   { return $timeExec;}
+	public getChaineNb()       { return $chaineNb;}
+	public getChaineTri()      { return $chaineTri;}
+	public getName()           { return $name;}
+
+	public setTimeExec($time)  { $this->timeExec($time);}
+	public setChaineNb($tab)   { $this->chaineNb($tab);}
+	public setChaineTri($tab)  { $this->chaineTri($tab);}
+	public setName($name)      { $this->name($name);}
+}
 ?>
