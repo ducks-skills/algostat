d3dd189a7928453c8ad730b5f7ee605a29268877
Martin Barreau <martin.barreau@gmail.com>
2018-01-10 11:39:38 +0100

    Adding materialize and simple homepage

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-	echo "Kaboom !\n";
-?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>AlgoStat</title>
+		<link rel="stylesheet" href="/css/materialize.min.css">
+	</head>
+	<body>
+		<nav>
+			<div class="nav-wrapper">
+				<a href="#" class="brand-logo">AlgoStat</a>
+				<ul id="nav" class="right hide-on-med-and-down">
+					<li><a href="benchmark.php">Benchmarks</a></li>
+				</ul>
+			</div>
+		</nav>
+		<h3>Bienvenue sur le site du projet AlgoStat !</h3>
+		<p>
+			Pour faire des tests sur les différents algorithmes proposés, rendez vous sur <a href="benchmark.php">cette page.</a>
+		</p>
+		<script type="text/javascript" src="/js/materialize.min.js"></script>
+	</body>
+</html>
