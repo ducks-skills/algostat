c3ef3422c9f5486acec84fdf2893220da0fdb94a
ducks-skills <briand78410@gmail.com>
2018-01-31 13:35:35 +0100

    lol

--- analyse.php
+++ analyse.php
@@ -69,8 +69,8 @@ if (isset($_POST['tri']))
 			</div>
 			</form>
 		</div>
-		<script type="text/javascript" src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/materialize.min.js"></script>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
 		<script type="text/javascript">
 		$(document).ready(function() {
 			$('select').material_select();
